ee0a099bffb58c8d68549924a5a68d701d957256
Mathilde <mgaden15@student.aau.dk>
2018-10-16 11:48:37 +0200

    script for building the GPR

---
+++ BuildModel.m
@@ -0,0 +1,10 @@
+clc; clear; close all;
+%% ST7 - Gaussian Process Regression (GPR)
+%This script uses data collected using the MATLAB-app
+%'CollectTrainingData', to build a GPR-model.
+
+%% Loading data
+uiopen('*.mat')
+
+
+%% Feature extraction and arrangement in predictors and target
