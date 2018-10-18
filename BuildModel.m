dbd258231157f62d8410de322bf4febd962d6b84
Mathilde <mgaden15@student.aau.dk>
2018-10-18 10:43:20 +0200

    Strukturering af EMG og beregning af RMS

--- BuildModel.m
+++ BuildModel.m
@@ -9,43 +9,47 @@ clc; clear; close all;
 fileFolder = dir(uigetdir);
 fileFolder = fileFolder.folder;
 
-% Check to make sure that folder actually exists.  Warn user if it doesn't.
-if ~isdir(fileFolder.folder)
-  errorMessage = sprintf('Error: The following folder does not exist:\n%s', fileFolder);
-  uiwait(warndlg(errorMessage));
-  return;
-end
-
 % Get a list of all .mat-files in the folder.
 listOfFiles = dir(fullfile(fileFolder, '*.mat'));
 % Load the files 
+EMG= []; RMS = [];
 for k = 1 : length(listOfFiles)
-  eval(['load ' listOfFiles(k).name]);
+  %data{k}= eval(['load ' listOfFiles(k).name]);
+  %sprintf('data%d', k) = load(listOfFiles(k).name);
+  %data = 
+  %filename = sprintf('data%d', k);
+ 
+  load(listOfFiles(k).name, 'emg');
+  data{k} = emg.values;
+  EMG = [EMG; data{1,k}];
+  RMS = [RMS rms(data{1,k}, 20, 15, 0)];
 end
+clear emg;
 
-
-
-% EMG
-% RMS= 
 % GeneratedProfile = 
 
-
+figure(1)
+title('EMG-data, RMS og Generated profile');
+subplot(2,1,1)
+plot(EMG)
+subplot(2,1,2)
+plot(RMS)
 
 
 %% Feature extraction and arrangement in predictors and target
 
 %Predictor - RMS of each EMG-channel of the myo armband for 25%, 50%, 75% of MVC. 
-x_flex
-x_ext
-x_rd 
-x_ud
-x_pro
-x_sup
-
-%Target values - The generated profile. 
-y_flex
-y_ext
-y_rd
-y_ud
-y_pro
-y_sup
+% x_flex
+% x_ext
+% x_rd 
+% x_ud
+% x_pro
+% x_sup
+% 
+% %Target values - The generated profile. 
+% y_flex
+% y_ext
+% y_rd
+% y_ud
+% y_pro
+% y_sup
