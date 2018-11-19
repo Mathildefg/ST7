21a4aff29bea75d702ad1368f4358e57503a427c
Benjaminrg95 <Benjamin.goethler@gmail.com>
2018-11-19 09:19:19 +0100

    Passer nu

--- preTest.m
+++ preTest.m
@@ -35,7 +35,7 @@ end
 % In this txt file target consist of 24 different positions all having the
 % same width and all the positions are doubled and randomized, resulting in
 % 48 random target positions.
-fileID = fopen('Fitt_real.txt','r');                
+fileID = fopen('Fitt_real_ny.txt','r');                
 Target = fscanf(fileID,'%f %f %f %f',[4 inf])';     %fscanf reads data from the fileID, 
                                                     %with format %f = floating-point numbers. 
                                                     %Reads 4 rows and to the end of the file in rows
@@ -51,7 +51,10 @@ threshold = 0.1; % Threshold
 W = Target(1,3); % Width or difficulty of the target
 Dwell = 1; % Dwell time [s]
 N = Time/dt; % Number of iterations
-target_times = 16; % Number of targets to hit for learning (randomly chosen)
+target_times = 10; % Number of targets to hit for learning
+dof1 = 0; dof2 = 0; dof3 = 0; dof4 = 0; dof5 = 0; dof6 = 0; dofA = 0; dofB = 0; dofC = 0;
+prevdofC = 0; nydofC = 0;
+xaksen = [dof1, dof2, dof3, dof4, dof5, dof6, dofA, dofB, dofC];
 
 % Two algorithms
 algo_s = ["GPR","LR"];
@@ -86,12 +89,64 @@ sysB = sysDisc.B;               %sysB is set to be the system discrete at B
 sysC = sysDisc.C;               %sysC is set to be the system discrete at C
 sysD = sysDisc.D;               %sysD is set to be the system discrete at D
 sysState = zeros(size(sysB));   %Creates an array of only zeros with the size of sysB, called sysState.
+%% Calculate the confidence baseline 
+% tilføjet af Steff
+baseCI_1 = mean(o1(:,2))-mean(o1(:,1)); %Den gennemsnitlige forskel mellem nedre og øvre CI
+baseCI_2 = mean(o2(:,2))-mean(o2(:,1));
+baseCI_3 = mean(o3(:,2))-mean(o3(:,1));
+baseCI_4 = mean(o4(:,2))-mean(o4(:,1));
+baseCI_5 = mean(o5(:,2))-mean(o5(:,1));
+baseCI_6 = mean(o6(:,2))-mean(o6(:,1));
 
 
 %% Build figure
 close all
+%plot for dof
+bargraph = figure;
+set(bargraph, 'units', 'normalized','position', [0 0 0.3 0.3],'menubar', 'none','renderer','painters','name','BarDoF','numbertitle','off','Color','w')
+b = bar(xaksen);
+ylim([-0.5 0.5]);
+
 hfig = figure;
 set(hfig, 'units', 'normalized','position', [0 0 1 1],'menubar', 'none','renderer','painters','name','Experiment','numbertitle','off')
+
+%Barplots of the confidence - tilføjet af Mat
+subplotConfi = subplot(1,2,1, 'position', [0.02 0.02 0.22 0.95]);
+axis([-1 1 -1 1])
+set(subplotConfi,'color', [.98,.98,.98], 'xtick', 0, 'ytick', 0);
+
+htext_ext = text(-0.70, 0.90, {'Extension'},'fontsize', 16, 'color', 'k'); 
+con_ext = rectangle('Position',[-0.70 0.35 0.5 0.5],'EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+htext_Flex = text(0.20, 0.90, {'Flexion'},'fontsize', 16, 'color', 'k');
+con_flex = rectangle('Position',[0.20 0.35 0.5 0.5],'EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+htext_rd = text(-0.70, 0.25, {'Radial Dev.'},'fontsize', 16, 'color', 'k');
+con_rd = rectangle('Position',[-0.70 -0.30 0.5 0.5],'EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+htext_Ud = text(0.20, 0.25, {'Ulnar Dev.'},'fontsize', 16, 'color', 'k');
+con_ud = rectangle('Position',[0.20 -0.30 0.5 0.5],'EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+htext_sup = text(-0.70, -0.40, {'Open'},'fontsize', 16, 'color', 'k'); 
+con_sup = rectangle('Position',[-0.70 -0.95 0.5 0.5],'EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+htext_pro = text(0.20, -0.40, {'Close'},'fontsize', 16, 'color', 'k'); 
+con_pro = rectangle('Position',[0.20 -0.95 0.5 0.5],'EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+%Plotting the confidence (fill) - tilføjet af Mat
+%The fill will represent the confidence by varying the height of a green
+
+ext_fill = 0; flex_fill= 0; rd_fill= 0; ud_fill= 0; sup_fill=0; pro_fill=0;
+con_ext_fill = rectangle('Position',[-0.70 0.35 0.5 ext_fill],'FaceColor','green','EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+con_flex_fill = rectangle('Position',[0.20 0.35 0.5 flex_fill],'FaceColor','green','EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+con_rd_fill = rectangle('Position',[-0.70 -0.30 0.5 rd_fill],'FaceColor','green','EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+con_ud_fill = rectangle('Position',[0.20 -0.30 0.5 ud_fill],'FaceColor','green','EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+con_sup_fill = rectangle('Position',[-0.70 -0.95 0.5 sup_fill],'FaceColor','green','EdgeColor',[.4,.4,.4],'Linewidth',1.2);
+con_pro_fill = rectangle('Position',[0.20 -0.95 0.5 pro_fill],'FaceColor','green','EdgeColor',[.4,.4,.4],'Linewidth',1.2); hold on;
+
+%Subplot for fitts law
+subplotFitts = subplot(1,2,2, 'position', [0.25 0.02 0.65 0.95]);
+
 % Add target marker
 htarget = plot(Target(1,1), Target(1,2), 'ro','markersize',Target(1,4),'MarkerFaceColor', [0.9100 0.4100 0.1700], 'linewidth', 2, 'buttondownfcn', 'uiresume'); hold on
 % Add target cross
@@ -146,29 +201,61 @@ for ii = 1:N
     algo = algo_s(idx);    %set the algorithm to be either algo_s(1) or (2) depending on randperm.
     switch algo
         case "GPR"         %if algorithm is Gaussian Process Regression
-            [dof1,p1] = predict(gprMdl_dof1,rms_data(N,:));
-            [dof2,p2] = predict(gprMdl_dof2,rms_data(N,:));
-            [dof3,p3] = predict(gprMdl_dof3,rms_data(N,:));
-            [dof4,p4] = predict(gprMdl_dof4,rms_data(N,:));
+            [dof1,p1,ci1] = predict(gprMdl_dof1,rms_data(N,:));
+            [dof2,p2,ci2] = predict(gprMdl_dof2,rms_data(N,:));
+            [dof3,p3,ci3] = predict(gprMdl_dof3,rms_data(N,:));
+            [dof4,p4,ci4] = predict(gprMdl_dof4,rms_data(N,:));
+            [dof5,p5,ci5] = predict(gprMdl_dof5,rms_data(N,:));
+            [dof6,p6,ci6] = predict(gprMdl_dof6,rms_data(N,:));
+            dataB.p(ii,1:6)=[p1,p2,p3,p4,p5,p6];
             
         case "LR"          %if algorithm is Linear regression
               dof1 = predict(LRmdl_1,rms_data(N,:));
               dof2 = predict(LRmdl_2,rms_data(N,:));
               dof3 = predict(LRmdl_3,rms_data(N,:));
               dof4 = predict(LRmdl_4,rms_data(N,:));
+              dof5 = predict(LRmdl_5,rms_data(N,:));
+              dof6 = predict(LRmdl_6,rms_data(N,:));
     end
     
-      % Predictions are ranging [0 1]. Set to [-1 1] for x and y
-    if dof1 >= dof2                 %if dof1 is bigger than or equal to dof2
-        dofA = -dof1;               %put dofA to be -dof1
+    
+      %% confidence interval istedet for dof1
+    nyci1 = ci1(1)+ci1(2);
+    nyci2 = ci2(1)+ci2(2);
+    nyci3 = ci3(1)+ci3(2);
+    nyci4 = ci4(1)+ci4(2);
+    nyci5 = ci5(1)+ci5(2);
+    nyci6 = ci6(1)+ci6(2);
+     
+    if  nyci1 <= nyci2 %hvis confidence interval for ext er mindre end flex
+        dofA = -nyci1; %sæt dofA til at være -confidence interval for ext
     else
-        dofA = dof2;                %else put dofA to be dof2
+        dofA = nyci2; %elllers sæt til confidence interval for flex
     end
-    if dof3 >= dof4                 %if dof3 is bigger than or equal to dof4
-        dofB = dof3;                %put dofB to be dof3                            Why is this reverse from previous MYO4?
+    if nyci3 <= nyci5 %hvis confidence int for rad er mindre end uln
+        dofB = -nyci3; %sæt dofB til at være -con int for rad
     else
-        dofB = -dof4;               %else dofB to be -dof4                          Why is this reverse from previous MYO4?
+        dofB = nyci5; %ellers sæt til at conf int for uln
     end
+    
+    %cursor size = dofC. bliver ikke brugt lige pt til noget
+    if nyci4 <= nyci6 %hvis confidence interval for 3 er mindre end 5
+        dofC = -nyci4;%sæt dofC til at være -ci3
+    else
+        dofC = nyci6;
+    end
+
+%       % Predictions are ranging [0 1]. Set to [-1 1] for x and y
+%     if dof1 >= dof2                 %if dof1 is bigger than or equal to dof2
+%         dofA = -dof1;               %put dofA to be -dof1
+%     else
+%         dofA = dof2;                %else put dofA to be dof2
+%     end
+%     if dof3 >= dof4                 %if dof3 is bigger than or equal to dof4
+%         dofB = dof3;                %put dofB to be dof3                            Why is this reverse from previous MYO4?
+%     else
+%         dofB = -dof4;               %else dofB to be -dof4                          Why is this reverse from previous MYO4?
+%     end
     % Use movement thresholds for minima and maxima
     if abs(dofB) < threshold            %if the absolute value of dofB is lower than the threshold
         dofB=0;                         %set dofB to be equal to 0
@@ -191,6 +278,39 @@ for ii = 1:N
         
     % Update the cursor position
     set(hpos, 'xdata', sysOut(1), 'ydata', sysOut(2));
+    drawnow
+    
+    % Update Bargraph
+    xaksen = [dof1, dof2, dof3, dof4, dof5, dof6, dofA, dofB, dofC, prevdofC, nydofC];
+    b.YData = xaksen;
+    
+    % Update confidence bars
+    
+    %ext_fill = max(0.5- ci1(:,2)-dof1,0);
+    ext_fill = min(max(0.5*(1-(((ci1(:,2)-ci1(:,1))-baseCI_1)/baseCI_1)),0),0.5);
+    set(con_ext_fill, 'Position', [-0.70 0.35 0.5 ext_fill]);
+    
+    %flex_fill = max(0.5- ci2(:,2)-dof2,0);
+    flex_fill = min(max(0.5*(1-(((ci2(:,2)-ci2(:,1))-baseCI_2)/baseCI_2)),0),0.5);
+    set(con_flex_fill, 'Position', [0.20 0.35 0.5 flex_fill]);
+    
+    %rd_fill = max(0.5- ci4(:,2)-dof4,0);
+    rd_fill = min(max(0.5*(1-(((ci4(:,2)-ci4(:,1))-baseCI_4)/baseCI_4)),0),0.5);
+    set(con_rd_fill, 'Position', [-0.70 -0.30 0.5 rd_fill]);
+    
+    %ud_fill = max(0.5- ci6(:,2)-dof6,0);
+    ud_fill = min(max(0.5*(1-(((ci6(:,2)-ci6(:,1))-baseCI_6)/baseCI_6)),0),0.5);
+    set(con_ud_fill, 'Position', [0.20 -0.30 0.5 ud_fill]);
+    
+    %pro_fill = max(0.5- ci3(:,2)-dof3,0);
+    pro_fill = min(max(0.5*(1-(((ci3(:,2)-ci3(:,1))-baseCI_3)/baseCI_3)),0),0.5);
+    set(con_pro_fill, 'Position', [0.20 -0.95 0.5 pro_fill]);
+    
+    %sub_fill = max(0.5-ci5(:,2)-dof5,0);
+    sup_fill = min(max(0.5*(1-(((ci5(:,2)-ci5(:,1))-baseCI_5)/baseCI_5)),0),0.5);
+    set(con_sup_fill, 'Position', [-0.70 -0.95 0.5 sup_fill]);
+    
+    
     drawnow
     
     % Update target position and keep track of the score, fails and time
